d70c0813da2e7bdc60f964fd8e5d2ce047867872
swxctx <swxctx@gmail.com>
2023-05-04 17:21:15 +0800

    add 常量

---
+++ lesson3/lesson3.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*const a string = "lesson3"
+	fmt.Println(a)*/
+
+	const a = "lesson3"
+	fmt.Println(a)
+
+	const b = 1
+	fmt.Println(b)
+
+	const c = 1.005
+	fmt.Println(c)
+
+	const d = true
+	fmt.Println(d)
+}
