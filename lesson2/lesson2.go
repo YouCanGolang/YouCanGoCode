34e11810d6d918de8af8c1bd7944293dbbf35fb2
swxctx <swxctx@gmail.com>
2023-05-04 16:56:56 +0800

    add 变量

---
+++ lesson2/lesson2.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*var a string
+	fmt.Println(a)*/
+
+	/*var a = "lesson2"
+	a = "lesson2"
+	fmt.Println(a)*/
+
+	/*var a string = "lesson2"
+	fmt.Println(a)*/
+
+	/*var a = "lesson2"
+	fmt.Println(a)*/
+
+	a := "lesson2"
+	fmt.Println(a)
+
+	var b int = 1
+	fmt.Println(b)
+
+	var c int32 = 1
+	fmt.Println(c)
+
+	var d int64 = 1
+	fmt.Println(d)
+
+	var e float32 = 1.001
+	fmt.Println(e)
+
+	var f float64 = 1.001
+	fmt.Println(f)
+
+	var g bool = true
+	fmt.Println(g)
+}
