28788af45d041a4b28d9400780a539407776ed30
swxctx <swxctx@gmail.com>
2024-08-08 11:50:15 +0800

    add 切片

---
+++ lesson8/lesson8.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a []int
+	a = append(a, 1)
+	a = append(a, 2, 3)
+	a = append(a, []int{4, 5}...)
+	fmt.Println(a[0])
+
+	var b = make([]int, len(a))
+	copy(b, a)
+	fmt.Println(b[0])
+	fmt.Println(b[1:3])
+
+	/*var c []string
+	var d []int = make([]int, 3)
+	var e []int = make([]int, 3, 4)*/
+}
